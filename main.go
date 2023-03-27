7d703db001a5ec47b55a09529cceeee883c85b56
Eric Hagman <eric@instawork.com>
2023-03-27 12:49:31 -0600

    update

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Instawork/terraform-provider-imgixyz/internal"
-)
-
 func main() {
 	// plugin.Serve(&plugin.ServeOpts{
 	// 	ProviderFunc: func() *schema.Provider {
 	// 		return internal.Provider()
 	// 	},
 	// })
-	client := internal.ImgixClient{}
-	client.SetAuthToken("***REMOVED***")
-	resp, err := client.GetSourceByID("63059169e12941a3897eca9e")
-	fmt.Println(resp)
-	fmt.Println(err)
 }
